36e581865044bd0dd34e275ec66aeca299436955
Ya Zhuang <zhuangya@users.noreply.github.com>
2020-03-13 11:00:38 +0800

    feat: cache leetcode api response
    ref: https://developers.cloudflare.com/workers/about/using-cache

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ addEventListener("fetch", event => {
 const handleRequest = async request => {
   const url = new URL(request.url);
   const [id] = url.pathname.split("/").slice(-1);
-  const response = await fetch("https://leetcode.com/api/problems/algorithms/");
+  const response = await fetch("https://leetcode.com/api/problems/algorithms/", { cf: { cacheEverything: true } });
   const body = await response.json();
   const localMapping = body.stat_status_pairs.reduce((soFar, current) => {
     return {
